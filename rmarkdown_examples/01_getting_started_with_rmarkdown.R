cbc4e77cd67c9ecf35cd62f92d4fada67a4cffb7
Thaliehln <helene.langet@swisstph.ch>
2022-08-15 00:38:19 +0300

    Update 01_getting_started_with_rmarkdown.R

--- rmarkdown_examples/01_getting_started_with_rmarkdown.R
+++ rmarkdown_examples/01_getting_started_with_rmarkdown.R
@@ -1,11 +1,2 @@
-# 01. Getting start with R Markdown
 # Install the rmarkdown package
-# Check if the package `rmarkdown` is already installed.
-
-rmarkdown_is_installed <- ("rmarkdown" %in% installed.packages())
-
-# If the package `rmarkdown` is not already installed, download and install it.
-
-if (!rmarkdown_is_installed){
-  install.packages("rmarkdown")
-}
+install.packages("rmarkdown")
