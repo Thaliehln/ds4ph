dccb3dd734d0aab7464cbc016283d4e24ed548bd
Thaliehln <helene.langet@swisstph.ch>
2022-08-17 10:11:36 +0300

    Document install packages

--- 01_getting_started_with_rmarkdown/01_install_packages.R
+++ 01_getting_started_with_rmarkdown/01_install_packages.R
@@ -1,11 +1,34 @@
+###################
+# Dynamic reports #
+###################
+
 # Install the rmarkdown package
 if (!require("rmarkdown")) install.packages("rmarkdown")
+
+# Install the knitr package
+if (!require("knitr")) install.packages("rmarkdown")
+
+#########
+# Stata #
+#########
 
 # Install the RStata package
 if (!require("RStata")) install.packages("RStata")
 
-# Install usethis
-if (!require("usethis")) install.packages("usethis")
+###########################
+# General data management #
+###########################
 
 # Install tidverse
 if (!require("tidyverse")) install.packages("tidyverse")
+
+# Tidyverse includes many packages for tidy data wrangling and presentation
+# haven: import and Export 'SPSS', 'Stata' and 'SAS' files.
+# ggplot2: data visualization
+
+######################
+# Automate workflows #
+######################
+
+# Install usethis
+if (!require("usethis")) install.packages("usethis")
