92603224fd6d7bc1ec35f0ec047ed85e75e7ca6e
Thaliehln <helene.langet@swisstph.ch>
2022-09-13 23:30:36 +0300

    Minor improvements

--- workshop_sept_2022/process_missing_data_4_stata.R
+++ workshop_sept_2022/process_missing_data_4_stata.R
@@ -1,12 +1,14 @@
 library(haven)
 
-df <- haven::read_dta("dataset1.dta")
+df <- haven::read_dta("dataset10.dta")
 
 for (colname in names(df)) {
   if (is.character(df[[colname]])) {
     df[[colname]] <- as.factor(df[[colname]])
   }
 }
+
+haven::write_dta(df, "dataset10.dta")
 
 df <- mtcars
 
