a79606d5366c96b7f2d236f51878ed19d204c569
Thaliehln <helene.langet@swisstph.ch>
2022-09-23 18:44:10 +0300

    Update workshop material

---
+++ book/save_quarto.R
@@ -0,0 +1,38 @@
+- part: "------------ DAY 1 ------------"
+chapters:
+  - tools_for_reproducible_data_science.qmd
+- version_control_with_git.qmd
+- dynamic_documents.qmd
+- create_first_quarto_doc.qmd
+- use_stata_python_with_quarto.qmd
+- import_data.qmd
+- manipulate_data.qmd
+- share_and_collaborate_with_git.qmd
+- part: "------------ DAY 2 ------------"
+chapters:
+  - data_science_for_public_health.qmd
+- malaria_case_study_01.qmd
+- malaria_case_study_02.qmd
+- part: "------------ DAY 3 ------------"
+chapters: 
+  - big_data_and_artificial_intelligence.qmd
+- ds4ph_day3_session01_practical_session.qmd
+
+appendices:
+  - installations.qmd
+- quarto_vs_rmarkdown.qmd
+- datasets.qmd
+- useful_resources.qmd
+
+### How to connect your GitHub account to GitHub Desktop?
+
+#### Authenticating to GitHub
+
+To connect to GitHub Desktop with GitHub, you will need to authenticate your GitHub account.
+
+1.  Use the File menu, then click Options.
+2.  In the Options window, select Accounts.
+3.  To the right of "GitHub.com," click Sign in.
+4.  In the "Sign in Using Your Browser" pane, click Continue With Browser. GitHub Desktop will open your default browser.
+5.  To authenticate to GitHub, type your GitHub.com credentials and click Sign in. Alternatively, if you were already signed in to GitHub, follow the prompts to return to GitHub Desktop to finish authenticating.
+6.  After GitHub authenticates your account, follow the prompts to return to GitHub Desktop.
